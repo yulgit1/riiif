fb3e62b43444583e51845b3913505cb98eaab83c
Eric James <eric.james@yale.edu>
2014-11-19 10:34:43 -0500

    cleaned akubra resolver

--- lib/riiif/akubra_system_file_resolver.rb
+++ lib/riiif/akubra_system_file_resolver.rb
@@ -2,16 +2,8 @@ require 'digest'
 require 'cgi'
 module Riiif
   module AkubraSystemFileResolver
-    #mattr_accessor :root, :base_path, :input_types
-
-    #self.root = File.expand_path(File.join(File.dirname(__FILE__), '../..'))
-    #self.base_path = File.join(root, 'spec/samples')
-    #self.input_types = %W{png jpg tiff jp jp2}
-	
-    #self.pathroot = "/usr/local/fedora/data/datastreamStore/"
     mattr_accessor :pathroot, :imagetype, :akubraconfig
-	#self.pathroot = "/hydra_store/fedora/fedora_tomcat/fedora_data/datastreamStore_dest/"
-	self.pathroot = "/hydra_store/fedora/fedora_tomcat/fedora_data/datastreamStore_dest/fakedefault"
+	self.pathroot = "/yourfedora/data/datastreamStore/"
     self.imagetype = "jp2"
 	self.akubraconfig = [[0,2],[2,2],[4,1]]
 
@@ -30,7 +22,6 @@ module Riiif
       md5 = Digest::MD5.new
       md5.update fullpid
       digest = md5.hexdigest
-      #directorystr = digest[0,2] + "/" + digest[2,2] + "/" +digest[4,1] + "/"
 	  directorystr = ""
 	  akubraconfig.each { |a| directorystr << digest[a[0],a[1]] << "/" }
       filename = CGI.escape(fullpid)
